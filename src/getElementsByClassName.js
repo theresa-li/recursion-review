a9277635e3a31fcf0506e580d400a10510bc4797
theresa-li <tli2@cca.edu>
2019-02-19 15:31:06 -0800

    In Progress

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,26 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
+/*
+     document.body
+  childNodes
 
+*/
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, node) {
   // your code here
+  var results = [];
+  var bodyHTML = node || document.body;
+  
+  
+  for (var i = 0; i < bodyHTML.childNodes.length; i++) {
+    if (bodyHTML.childNodes[i].className === className) {
+      results.push(bodyHTML.childNodes[i]);
+    }
+  
+    if (bodyHTML.childNodes[i].hasChildNodes()) {
+      results.push(getElementsByClassName(className, bodyHTML.childNodes[i]));
+    }
+  }
+  return results;
 };
